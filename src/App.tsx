e21d4ac7a9063c80a4ea19555e62e9229e8b002f
cmnwankw-cmu-F24 <cmnwankw@andrew.cmu.edu>
2024-10-15 10:11:35 +0200

    ft-uc1(added the user management interface)

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ActivateAccount from './pages/ActivateAccount';
+import UserManagement from './pages/UserManagement';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -41,6 +42,14 @@ const App: React.FC = () => {
                   element={
                     <PrivateRoute>
                       <Dashboard />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/users"
+                  element={
+                    <PrivateRoute>
+                      <UserManagement />
                     </PrivateRoute>
                   }
                 />
